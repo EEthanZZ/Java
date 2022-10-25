25718dd2a372660e590a4c606b2bae57b281c973
EEthanZZ <yczhu@yahoo.com>
2022-10-25 22:32:55 +1100

    build up the arraylist

--- src/game/game_create.java
+++ src/game/game_create.java
@@ -3,8 +3,15 @@ package game;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class game_create extends JFrame implements ActionListener {
+
+    private ArrayList<Character> cha;
+    private ArrayList<Location> loc;
+
+
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
