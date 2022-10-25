3c52935b187af0073478b542302eac0e944d6751
EEthanZZ <yczhu@yahoo.com>
2022-10-25 21:25:57 +1100

    create the JFrame

---
+++ src/game/game_create.java
@@ -0,0 +1,12 @@
+package game;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class game_create extends JFrame implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
