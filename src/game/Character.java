35a21a8173db2502d56999f284f25a832ce453c1
EEthanZZ <yczhu@yahoo.com>
2022-10-25 21:23:33 +1100

    character class built

---
+++ src/game/Character.java
@@ -0,0 +1,14 @@
+package game;
+
+public class Character {
+    private int char_id;
+
+    private String char_name;
+
+    static int nextId = 1;
+
+    public Character (String char_name){
+        this.char_name=char_name;
+        char_id = nextId++;
+    }
+}
