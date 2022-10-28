7a0f108e58b516f3d229b0c61faada6c82c9ec80
EEthanZZ <yczhu@yahoo.com>
2022-10-28 14:41:01 +1100

    connect java to database named as rmitlab

--- src/game/db.java
+++ src/game/db.java
@@ -2,25 +2,33 @@ package game;
 
 
 import javax.xml.transform.Result;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class db {
     public static void main(String[] args) throws SQLException {
 //        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/", "root", "zycc0033");
-        if (connection == null) {
-            System.out.println("JDBC connection is not established");
-            return;
-        } else
-            System.out.println("Congratulations," +
-                    " JDBC connection is established successfully.\n");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/rmitlab", "root", "zycc0033");
+
+        Statement statement = connection.createStatement();
+        String aa = "select * from student";
+        ResultSet result = statement.executeQuery(aa);
+
+        boolean flag = false;
+        while (result.next()){
+            flag = true;
+            System.out.println(result.getInt(1) + " " + result.getString(2));
+        }
+        if (flag == true){
+            System.out.println("record received and displayed");
+        }
+        else {
+            System.out.println("records not found");
+        }
+
+        result.close();
         connection.close();
-//        Statement statement = connection.createStatement();
 //
-//        Result result = statement.executeQuery("select * from people");
+//
 
 
 
