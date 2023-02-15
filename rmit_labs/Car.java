ea9092162acede14fd186c3ad769a4f9ae272e05
EEthanZZ <yczhu@yahoo.com>
2023-02-15 13:41:07 +1100

    practise_1_complete

---
+++ rmit_labs/Car.java
@@ -0,0 +1,53 @@
+package rmit_labs;
+
+public class Car {
+    private String make;
+    private String model;
+    private int year;
+    private int carID;
+    static int nextID = 1;
+
+    public Car() {
+        carID = nextID++;
+    }
+
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.carID = nextID++;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getCarID() {
+        return carID;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String toString() {
+        return "Car ID: " + carID + ", Make: " + make + ", Model: " + model + "Made in year of "
+                + year;
+    }
+}
