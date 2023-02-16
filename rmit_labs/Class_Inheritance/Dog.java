f9a15c1bf3dabd0c64478c475d93f42935530de1
EEthanZZ <yczhu@yahoo.com>
2023-02-16 14:34:56 +1100

    SubClass Dog

---
+++ rmit_labs/Class_Inheritance/Dog.java
@@ -0,0 +1,36 @@
+package rmit_labs.Class_Inheritance;
+
+public class Dog extends Pet {
+    private String breed;
+
+    public Dog() {
+        System.out.println("SubClass is called");
+    }
+
+    public Dog(String name, int age) {
+        super(name, age);
+        System.out.println("SubClass is called");
+    }
+
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+        System.out.println("SubClass is called");
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void speak(String sound) {
+        System.out.println(name + "says" + sound);
+    }
+
+    public String toString() {
+        return super.toString() + name + "is " + breed + ".";
+    }
+}
