bd4e0883b7567c8c827d98d128022dc23cd8812b
EEthanZZ <yczhu@yahoo.com>
2023-02-19 21:23:36 +1100

    date&time format

--- rmit_labs/Class_Inheritance/JavaTimeModule/Time.java
+++ rmit_labs/Class_Inheritance/JavaTimeModule/Time.java
@@ -1,6 +1,7 @@
 package Class_Inheritance.JavaTimeModule;
 
-import java.time.*;;
+import java.time.*;
+import java.time.format.*;;
 
 public class Time {
     public static void main(String[] args) {
@@ -12,6 +13,10 @@ public class Time {
 
         LocalDateTime myDateTime = LocalDateTime.now();
         System.out.println(myDateTime);
+
+        DateTimeFormatter formattedDateTime = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formatted = myDateTime.format(formattedDateTime);
+        System.out.println(formatted);
     }
 
 }
