ddd26b8577f93ba4cf4518f5c9102b83db81b6f5
EEthanZZ <yczhu@yahoo.com>
2023-02-24 10:16:58 +1100

    square array

--- rmit_labs/Array/SquareRun.java
+++ rmit_labs/Array/SquareRun.java
@@ -2,7 +2,12 @@ package Array;
 
 public class SquareRun {
     public static void main(String[] args) {
-        Square sq1 = new Square(3.2);
-        System.out.println(sq1.toString());
+        Square[] squares = new Square[3];
+        squares[0] = new Square(2.2);
+        squares[1] = new Square(3.3);
+        squares[2] = new Square(4.4);
+        for (int i = 0; i < squares.length; i++) {
+            System.out.println(squares[i].toString());
+        }
     }
 }
