5df017c49e80794f5b8fbc91517116ea568c18d4
EEthanZZ <yczhu@yahoo.com>
2023-02-24 12:40:18 +1100

    student input

--- rmit_labs/Array/StudentRun.java
+++ rmit_labs/Array/StudentRun.java
@@ -1,19 +1,24 @@
 package Array;
 
+import java.util.Scanner;
+
+import javax.print.attribute.standard.Sides;
+
 public class StudentRun {
     public static void main(String[] args) {
-        Student[] students = new Student[10];
-        students[0] = new Student("john", 22);
-        students[1] = new Student("mike", 33);
+        Student[] students = new Student[2];
+        Scanner input = new Scanner(System.in);
         for (int i = 0; i < students.length; i++) {
-            if (students[i] != null) {
-                System.out.println(students[i].toString());
-            }
+            System.out.println("Students name: ");
+            String name = input.nextLine();
+            System.out.println("Student number: ");
+            int number = input.nextInt();
+            students[i] = new Student(name, number);
+            input.nextLine();
+
+            System.out.println();
+
         }
-        String studentName = students[0].getName();
-        System.out.println(studentName);
-        students[1].setName("LLLLLLLLoke");
-        System.out.println(students[1].getName());
     }
 
 }
