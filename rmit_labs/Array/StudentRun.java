8e0e811fa303a12126f842de92241e6478360b78
EEthanZZ <yczhu@yahoo.com>
2023-02-24 13:45:37 +1100

    while loop

--- rmit_labs/Array/StudentRun.java
+++ rmit_labs/Array/StudentRun.java
@@ -2,23 +2,33 @@ package Array;
 
 import java.util.Scanner;
 
-import javax.print.attribute.standard.Sides;
-
 public class StudentRun {
     public static void main(String[] args) {
-        Student[] students = new Student[2];
-        Scanner input = new Scanner(System.in);
-        for (int i = 0; i < students.length; i++) {
-            System.out.println("Students name: ");
+        int count = 0; // to count elements(students) created
+        String choice = "y";
+        // create array object
+        Student[] students = new Student[5];
+        while (choice.equalsIgnoreCase("y")) {
+            // create Scanner
+            Scanner input = new Scanner(System.in);
+            // prompt and read
+            System.out.println("Student name:");
             String name = input.nextLine();
-            System.out.println("Student number: ");
+            System.out.println("Student number:");
             int number = input.nextInt();
-            students[i] = new Student(name, number);
-            input.nextLine();
-
-            System.out.println();
-
+            // create array element using constructor
+            students[count] = new Student(name, number);
+            // increment count
+            count++;
+            // check user choice
+            // create a Scanner to read choice
+            Scanner choiceInput = new Scanner(System.in);
+            System.out.println("Do you wish to proceed?(y/n)");
+            choice = choiceInput.nextLine();
         }
-    }
-
-}
+        // display array
+        students[2].setName("J");
+        for (int i = 0; i < count; i++)
+            System.out.println(students[i]);
+    }// main
+}// class
