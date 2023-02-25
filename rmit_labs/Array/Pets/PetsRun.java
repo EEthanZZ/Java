6d6d40fd0666ad7216afbb6c135bf0ac1dd8dd55
EEthanZZ <yczhu@yahoo.com>
2023-02-25 12:49:56 +1100

    Array print with for loop

---
+++ rmit_labs/Array/Pets/PetsRun.java
@@ -0,0 +1,27 @@
+package Array.Pets;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class PetsRun {
+    public static void main(String[] args) {
+        int count = 0;
+        Dogs dog1 = new Dogs("Dog1", 10, "bulldog");
+        count++;
+        Cats cat1 = new Cats("Cat1", 20, "blue");
+        count++;
+        Dogs dog2 = new Dogs("Dog2", 30, "bb");
+        count++;
+        Cats cat2 = new Cats("Cat2", 40, "grey");
+        count++;
+        // Create array of type of pets:
+        Pets[] pets_types = new Pets[5];
+        pets_types[0] = dog1;
+        pets_types[1] = cat1;
+        pets_types[2] = dog2;
+        pets_types[3] = cat2;
+        System.out.println("------------");
+        for (int i = 0; i < count; i++)
+            System.out.println(pets_types[i]);
+    }
+
+}
