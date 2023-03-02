0dcc152817e6da8f5badef7f0742aec7ac76d710
EEthanZZ <yczhu@yahoo.com>
2023-03-03 09:18:33 +1100

    random

--- rmit_labs/Array/SquareRun.java
+++ rmit_labs/Array/SquareRun.java
@@ -1,13 +1,22 @@
 package Array;
 
+import java.util.Random;
+
 public class SquareRun {
     public static void main(String[] args) {
-        Square[] squares = new Square[3];
-        squares[0] = new Square(2.2);
-        squares[1] = new Square(3.3);
-        squares[2] = new Square(4.4);
+        Square[] squares = new Square[7];
+        Random r = new Random();
         for (int i = 0; i < squares.length; i++) {
-            System.out.println(squares[i].toString());
+            double d = r.nextDouble();
+            if (i % 2 == 0)
+                squares[i] = new Square(d * 100);
+            else
+                squares[i] = new Square(3.3);
         }
+
+        for (int i = 0; i < squares.length; i++) {
+            System.out.printf("%8.2f\n", squares[i].getArea());
+        }
+
     }
 }
