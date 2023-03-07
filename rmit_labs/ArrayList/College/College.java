7df185ad557bc28b7073a626a692498e6afcc0f3
EEthanZZ <yczhu@yahoo.com>
2023-03-08 00:01:09 +1100

    College

---
+++ rmit_labs/ArrayList/College/College.java
@@ -0,0 +1,12 @@
+package ArrayList.College;
+import java.util.*;
+public class College {
+    ArrayList<College>courses = new ArrayList<College>();
+    private String cName;
+    Scanner input = new Scanner(System.in);
+
+    public College(String cName){
+        this.cName = cName;
+        
+    }
+}
---
+++ rmit_labs/ArrayList/College/Course.java
@@ -0,0 +1,31 @@
+package ArrayList.College;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Course {
+    private String cID;
+    private String cName;
+    ArrayList<Student> students;
+
+    public Course(){}
+    public Course(String cID, String cName){
+        this.cName = cName;
+        this.cID = cID;
+        students = new ArrayList<Student>();
+    }
+
+    public void addStudent(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Student Name:");
+        String name = input.nextLine();
+        System.out.println("Student ID: ");
+        int id = input.nextInt();
+        Student s = new Student(id, name);
+        students.add(s);
+    }
+
+    public void removeStudent(){
+        
+    }
+}
