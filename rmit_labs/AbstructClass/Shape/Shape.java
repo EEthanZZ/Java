db1091d9c6473ef1f689db2a898d616710eef970
EEthanZZ <yczhu@yahoo.com>
2023-03-18 23:18:46 +1100

    abstruct parent class

---
+++ rmit_labs/AbstructClass/Shape/Shape.java
@@ -0,0 +1,8 @@
+package AbstructClass.Shape;
+
+public abstract class Shape{
+    public Shape(){
+        System.out.println("SUper class is called");
+    }
+    public abstract double getArea();
+}
